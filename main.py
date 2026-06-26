b8bdce24a0048fd190f08579b27c359f832c9e36
overcO-Oked <216619665+overcO-Oked@users.noreply.github.com>
2026-06-26 22:40:37 +0600

    API Key Fix

--- main.py
+++ main.py
@@ -10,12 +10,7 @@ from pydantic import BaseModel
 # ===========================
 # Load environment variables
 # ===========================
-load_dotenv()
-
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-
-if not GEMINI_API_KEY:
-    raise RuntimeError("GEMINI_API_KEY not found in environment variables.")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "")
 
 client = genai.Client(api_key=GEMINI_API_KEY)
 
