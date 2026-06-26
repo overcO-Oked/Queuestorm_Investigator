61bbe65ff21caef8769f0670be6cc76bb989de03
overcO-Oked <216619665+overcO-Oked@users.noreply.github.com>
2026-06-26 23:54:29 +0600

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from google import genai
 from google.genai.types import GenerateContentConfig
 from pydantic import BaseModel
 
-local_dotenv()
+# local_dotenv()
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "")
 
